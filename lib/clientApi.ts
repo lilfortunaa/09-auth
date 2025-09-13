ef392f79e1c6ab1ed86f63b8e2c76248656fa052
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-09-13 16:49:19 +0200

    cors

--- lib/clientApi.ts
+++ lib/clientApi.ts
@@ -20,26 +20,17 @@ export interface FetchNotesResponse {
 }
 
 export async function fetchNotes({
-    page = 1,
-    perPage = 12,
-    search = '',
-    tag,
-}:FetchNotesParams):Promise<FetchNotesResponse> {
-    const params: Record<string,string | number> ={page,perPage};
+  page = 1,
+  perPage = 12,
+  search = '',
+  tag,
+}: FetchNotesParams): Promise<FetchNotesResponse> {
+  const params: Record<string,string | number> = { page, perPage };
+  if(search) params.search = search;
+  if(tag && tag !== 'All') params.tag = tag;
 
-    if(search) params.search = search;
-    if(tag && tag !== 'All') params.tag = tag;
-
-
-    const res: AxiosResponse<FetchNotesResponse> = await api.get ("/notes", {
-    params,
-  });
-
-   return {
-    ...res.data,
-    page,
-    perPage,
-  };
+  const res: AxiosResponse<FetchNotesResponse> = await api.get("/notes", { params });
+  return { ...res.data, page, perPage };
 }
 
 export interface CreateNoteDto {
@@ -60,19 +51,16 @@ export async function deleteNote(id: string): Promise<Note> {
 
 export async function fetchNoteById(id: string): Promise<Note> {
   const res: AxiosResponse<Note> = await api.get(`/notes/${id}`);
-  console.log("fetchNoteById response:", res.data);
   return res.data;
 }
 
-interface SignUp {
+export interface SignUp {
   email: string;
   password: string;
 }
 
 export async function signUp(data: SignUp): Promise<User> {
-  const res: AxiosResponse<User> = await api.post("/auth/register", data, {
-    withCredentials: true, 
-  });
+  const res: AxiosResponse<User> = await api.post("/auth/register", data);
   return res.data;
 }
 
@@ -82,9 +70,6 @@ export interface SignIn {
 }
 
 export async function signInUser(data: SignIn): Promise<User> {
-  const res: AxiosResponse<User> = await api.post("/auth/login", data, {
-    withCredentials: true, 
-  });
+  const res: AxiosResponse<User> = await api.post("/auth/login", data);
   return res.data;
 }
-
